36a86fc16afcedeeecac0bd1ffa53b4215d7a154
gulsevim-blbl <gulsevimblbl@gmail.com>
2025-12-04 17:08:37 +0300

    Routing

---
+++ Pages/Employees.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorpages.Pages;
+
+public class EmployeesModel : PageModel
+{
+       public void OnGet()
+    {
+
+    }
+
+}
